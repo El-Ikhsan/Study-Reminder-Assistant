6579e470321f53cf2fc0597a5f8f7740815bf822
El-Ikhsan <shadowex179@gmail.com>
2026-03-15 13:13:11 +0700

    refactor: simplify worker entrypoint to export app directl

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-import app from './app'
-import { setWorkerEnv } from './config/env'
+import app from "./app"
 
-export default {
-  async fetch(request: Request, env: any, ctx: any): Promise<Response> {
-    try {
-      // Set env per request (AMAN & BENAR)
-      setWorkerEnv(env)
-
-      return await app.fetch(request, env, ctx)
-    } catch (error) {
-      console.error('Worker error:', error)
-      return new Response(
-        JSON.stringify({
-          success: false,
-          message: 'Internal server error',
-        }),
-        {
-          status: 500,
-          headers: { 'Content-Type': 'application/json' },
-        }
-      )
-    }
-  },
-}
-
-export { app }
+export default app
